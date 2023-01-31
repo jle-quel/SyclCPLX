c80296dd6b554c46263a0f9cfe03e274d7038969
jle-quel <jeffersonlequellec@me.com>
2023-01-31 17:54:49 +0100

    15

--- tests/acos_complex.cpp
+++ tests/acos_complex.cpp
@@ -43,7 +43,10 @@ TEMPLATE_TEST_CASE("Test complex acos", "[acos]", double, float, sycl::half) {
     }
     Q.wait();
 
-    check_results(cplx_out[0], std_out, /*tol_multiplier*/ 6);
+    bool val = detail::almost_equal(cplx_out[0], std_out, 2 * SYCL_CPLX_TOL_ULP);
+    std::cout << "(" << cplx_out->real() << ", " << cplx_out->imag() << ") == (" << std_out.real() << ", " << std_out.imag() << ") : " << val << "\n";
+
+    check_results(cplx_out[0], std_out, /*tol_multiplier*/ 2);
   }
 
 
@@ -53,7 +56,11 @@ TEMPLATE_TEST_CASE("Test complex acos", "[acos]", double, float, sycl::half) {
   else
     cplx_out[0] = sycl::ext::cplx::cos<T>(sycl::ext::cplx::acos<T>(cplx_input));
 
-  check_results(cplx_out[0], std_out, /*tol_multiplier*/ 5);
+  bool val = detail::almost_equal(cplx_out[0], std_out, 2 * SYCL_CPLX_TOL_ULP);
+  std::cout << "(" << cplx_out->real() << ", " << cplx_out->imag() << ") == (" << std_out.real() << ", " << std_out.imag() << ") : " << val << "\n";
+  std::cout << "\n";
+
+  check_results(cplx_out[0], std_out, /*tol_multiplier*/ 2);
 
   sycl::free(cplx_out, Q);
 }
